b07caddd246822d6d4f13dc4c738323cffae00cb
lispincsclothing <scullingnull+1@gmail.com>
2015-07-18 21:42:10 -0700

    Added algorithm problems - sorted by name

---
+++ ruby/algo/matching-parens.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+# Write the function #has_matching_parens that takes a string as an argument and returns true if the string has valid parenthesis.
+#
+# ex.
+# str = “(I(really)love (al(g)or)ithms)” has_matching_parens(str) #=> true
+#
+# Bonus: How would you solve if there were [], (), and {}?
+
+def has_matching_parens(input_string)
+  stack = []
+  lookup_hash = {'{' => '}', '[' => ']', '('=>')'}
+  lookup_keys = lookup_hash.keys
+  lookup_values = lookup_hash.values
+
+  input_string.each_char do |input_char|
+    if lookup_keys.include? input_char
+      stack << input_char
+    elsif lookup_values.include? input_char
+      return false if stack.empty? || (lookup_hash[stack.pop] != input_char)
+    end
+  end
+  stack.empty?
+end
+
+p has_matching_parens("(((hello))")==false
+p has_matching_parens("((")==false
+p has_matching_parens(")(")==false
+p has_matching_parens("(())")==true
